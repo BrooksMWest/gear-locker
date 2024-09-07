d78c47f5079b510e59f07b99f4441337ba98477e
Brooks West <brooksmwest@hotmail.com>
2024-09-07 11:33:56 -0500

    fixed function name to avoid linting issue

---
+++ components/forms/gearForm.js
@@ -0,0 +1,202 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { getTypes } from '../../api/typeData';
+import { createGearItem, updateGear } from '../../api/gearData';
+
+const initialState = {
+  gearId: '',
+  ownerId: '',
+  gearName: '',
+  typeId: '',
+  maker: '',
+  acquiredOn: '',
+  acquiredFrom: '',
+  condition: '',
+  serialNumber: '',
+  description: '',
+  isArchived: '',
+  favorite: '',
+  wantToSell: '',
+  picture: '',
+};
+
+function GearForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const [types, setTypes] = useState([]);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getTypes(user.uid).then(setTypes);
+
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateGear(formInput).then(() => router.push(`/gear/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createGearItem(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateGear(patchPayload).then(() => {
+          router.push('/');
+        });
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} Gear</h2>
+
+      {/* GEAR NAME INPUT  */}
+      <FloatingLabel controlId="floatingInput1" label="Gear Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Gear Name"
+          name="gearName"
+          value={formInput.gearName}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* TYPE OF GEAR INPUT (DROPDOWN)  */}
+      <FloatingLabel controlId="floatingSelect" label="Type">
+        <Form.Select
+          aria-label="Type"
+          name="type_id"
+          onChange={handleChange}
+          className="mb-3"
+          key={formInput.type_id}
+          value={formInput.type_id}
+          required
+        >
+          <option value="">Select an Author</option>
+          {
+            types.map((type) => (
+              <option
+                key={type.firebaseKey}
+                value={type.firebaseKey}
+              >
+                {type.name}
+              </option>
+            ))
+          }
+        </Form.Select>
+      </FloatingLabel>
+
+      {/* MANUFACTURER INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="Maker" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Manufacturer"
+          name="maker"
+          value={formInput.maker}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* ACQUISITION DATE INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="acquiredOn" className="mb-3">
+        <Form.Control
+          type="date"
+          placeholder="Date Acquired"
+          name="acquiredOn"
+          value={formInput.acquiredOn}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* FROM WHOM/WHERE  INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="acquiredFrom" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter price"
+          name="price"
+          value={formInput.price}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      {/* CONDITION INPUT  */}
+
+      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter price"
+          name="price"
+          value={formInput.price}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+      {/* SERIAL # INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="Book Price" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter price"
+          name="price"
+          value={formInput.price}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* DESCRIPTION TEXTAREA  */}
+      <FloatingLabel controlId="floatingTextarea" label="Description" className="mb-3">
+        <Form.Control
+          as="textarea"
+          placeholder="Description"
+          style={{ height: '100px' }}
+          name="description"
+          value={formInput.description}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Gear</Button>
+    </Form>
+  );
+}
+
+GearForm.propTypes = {
+  obj: PropTypes.shape({
+    ownerId: PropTypes.string,
+    name: PropTypes.string,
+    maker: PropTypes.string,
+    acquiredOn: PropTypes.bool,
+    acquiredFrom: PropTypes.string,
+    condition: PropTypes.string,
+    serialNumber: PropTypes.string,
+    isArchived: PropTypes.bool,
+    favorite: PropTypes.bool,
+    wantToSell: PropTypes.bool,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+GearForm.defaultProps = {
+  obj: initialState,
+};
+
+export default GearForm;
